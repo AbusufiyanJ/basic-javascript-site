c906a0790b445249869c115220be95a9ea64b00d
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-17 23:55:46 +0530

    added image and place holder

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
-alert("!!!!!!!!!  Issue !!!!!!!!!");
 
+var myAge = 22;
 
-alert("hi hi");
+if(myAge > 30){
+    
+    document.write("ufff its my brother age");
+}
+ else if (myAge > 40){
+    
+    document.write("are u kidding me!!");
+    
+} else if(myAge == 22){
+    
+    document.write("you are right doode");
+} else{
+    
+    document.write("try again!!!!");
+}
+    
