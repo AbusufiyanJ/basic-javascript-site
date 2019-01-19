3a5527ce99575f3bfb2822c1293e3b00207b2fbb
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-19 23:43:50 +0530

    updated js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-var myAge = 22;
+/* var myAge = 22;
 
 if(myAge > 30){
     
@@ -15,5 +15,23 @@ if(myAge > 30){
 } else{
     
     document.write("try again!!!!");
-}
+} */
+
+function add() {
     
+    var myAge =document.getElementById("v1").value;
+    
+
+if (myAge < 18 || myAge > 30 ){
+    
+    
+    document.write( 'You can come,you cool doode');
+   
+    
+} else{
+    document.write("you aint coming");
+    
+} 
+   document.forms.content.result.value; 
+}
+
