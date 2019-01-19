5ca1791ddfba0e257818db92c603baa61ca08fb4
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-19 23:46:53 +0530

    updated js

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
 
-/* var myAge = 22;
 
-if(myAge > 30){
-    
-    document.write("ufff its my brother age");
-}
- else if (myAge > 40){
-    
-    document.write("are u kidding me!!");
-    
-} else if(myAge == 22){
-    
-    document.write("you are right doode");
-} else{
-    
-    document.write("try again!!!!");
-} */
 
 function add() {
     
