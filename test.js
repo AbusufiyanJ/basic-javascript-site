84938ae18b356bdf56314b0a156421ae1fa777e9
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-20 23:46:43 +0530

    include or change

--- test.js
+++ test.js
@@ -4,18 +4,18 @@
 /*function add() {
     
     var myAge =document.getElementById("v1").value;
-  */  
+*/  
 
-if (myAge < 18 || myAge > 30 ){
+var myAge = 25;
+
+if (myAge < 18 || myAge > 30 || myAge === 25){
     
     
-    document.write( 'You can come,you cool doode');
+    document.write( 'you aint coming');
    
     
 } else{
-    document.write("you aint coming");
+    document.write("You can come,you cool doode");
     
 } 
-   document.forms.content.result.value; 
-}
 
