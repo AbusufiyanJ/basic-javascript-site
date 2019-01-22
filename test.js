e67d77c24c974e4bd8cf8d9c0683fd63144e1831
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-22 23:38:36 +0530

    numbers

--- test.js
+++ test.js
@@ -3,25 +3,8 @@
     var myAge =document.getElementById("v1").value;
 */  
 
-/*function getAverage (a,b,c,d,e,f) {
-    
-    var average = (a + b +c +d + e + f )  /6;
-    console.log(average);
-    return average;
-    
-}
+var a = 5.2;
+var b = 5.3;
 
-var myresult = getAverage(7,8,9,10,11,12);
+var result = console.log("the result is " + (a+b));
 
-function logresult(){
-    
-    console.log("The average is " + myresult);
-}
-
-logresult(); */
-
-
-var a = "5" ;
-var b = 5;
-
-console.log(a+b);
