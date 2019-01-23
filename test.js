301e91838f3551f9b28930a40a632cffe13938cc
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-23 18:16:42 +0530

    created new javascript objects

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-var monkey = 'Nan\'kal\'oru abu "muttal" koranku';
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1]= "hello";
 
-if(monkey.indexOf("abu") === -1){
-    console.log("the Word abu is not in the string")
-    
-}else {
-    console .log("the word abu starts at the position:" + monkey.indexOf("abu"));
-}
+var myCar = new Object();
+myCar.maxspeed = 50;
+myCar.driver = "Abu";
+
+myCar.drive =function(){console.log("Now Driving : "  );};
+
+myCar.drive( );
+
+var myCar2 = {
+    maxSpeed: 70, 
+    driver: "sufiyan",
+    drive: function(speed, time)
+{ console.log( speed * time);} 
+ 
+};
+
+console.log(myCar2.maxSpeed);
+
+myCar2.drive(50,3);
