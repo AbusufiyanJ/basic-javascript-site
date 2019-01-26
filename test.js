698b5a85e3194377037cfe207c368e855094397a
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-01-26 23:48:47 +0530

    basic change

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-var car = function(maxspeed,driver){
-    
-     this.maxSpeed = maxspeed;
-    this.driver= driver;
-    this.drive = function(speed, time){
-        console.log(speed * time);
-};
-this.logDriver = function(){
-    console.log("driver name is " + this.driver);
-};
-    
-    this.show_speed =function(){
-        console.log("the car speed is " +this.maxSpeed);
-    };
-    
-}
+ var myPastDate = new Date( 2019 , 0 , 9, 10, 30, 15);
+var myfutureDate = new Date(2545 , 11 , 9, 10 ,30,15);
 
-var mycar = new car(30 , "kid");
-var mycar1 = new car(50 , "men");
-var mycar2 = new car(70 , "legend");
-var mycar3 = new car(120 , "ultra legend");
-
-mycar.drive(30 ,10);
-mycar2.show_speed() + mycar3.logDriver();
+     console.log(myPastDate);
+    console.log(myfutureDate.getFullYear());
