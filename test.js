4f53db381172781012b7bd63f9408fc53d81b2fe
unknown <abujafarullah1996@gmail.com>
2019-01-30 10:28:52 +0530

    rewritted the nested if in button

--- test.js
+++ test.js
@@ -2,18 +2,21 @@ var content =document.getElementById("content");
 var button =document.getElementById("content-button");
 
 button.onclick = function(){
-    if(content.className== "open2"){
-        content.className = "open3";
-        button.innerHTML = "show less";
-
-    } else if(content.className == "open3") {
+    if(content.className  == "")  {
         content.className = "open1";
-        button.innerHTML = "show more";
+        button.innerHTML = "show more if";
 
+    }else if(content.className == "open1") {
+        content.className = "open3";
+        button.innerHTML = "show less else if";
     }
-    else{
+    else if(content.className == "open3") {
         content.className = "open2";
-        button.innerHTML = "show more "
+        button.innerHTML = "show less else if 2";
+    }
+    else {
+        content.className = "";
+        button.innerHTML = "show more else";
     }
 
 };
